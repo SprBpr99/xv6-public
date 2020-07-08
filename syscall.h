d7c7bad42cf2d1e3039c03b41e30911c0a5b0743
Sepehr Babapour <shmah51@gmail.com>
2020-07-08 20:28:11 +0430

    part 2: fix syscall.h comment

--- syscall.h
+++ syscall.h
@@ -22,5 +22,5 @@
 #define SYS_close  21
 // Assign number 22 to SYS_waitx.
 #define SYS_waitx  22
-// Assign number 22 to SYS_set_priority.
+// Assign number 23 to SYS_set_priority.
 #define SYS_set_priority  23
